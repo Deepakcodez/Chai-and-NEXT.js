46976b6f795ffb55a2b088991f122fa8c1e6835b
Deepak <deepak2603dk@gmail.com>
2024-01-03 23:55:35 +0530

    first commit

---
+++ routing/src/app/user/page.js
@@ -0,0 +1,10 @@
+export default function user(){
+    return(
+        <>
+        <div className="container h-[50vh] bg-orange-400">
+        <h1 className="">i am user page</h1>
+
+        </div>
+        </>
+    )
+}
---
+++ routing/src/app/user/profile/page.js
@@ -0,0 +1,9 @@
+export default function userProfile(){
+    return(
+        <>
+        <div className="profile h-[50vh] bg-amber-500">
+         <h1>user profile page</h1>
+        </div>
+        </>
+    )
+}
